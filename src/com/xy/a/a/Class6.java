a180048741a4c000a7863869ec0ad8cf0185815b
Administrator <488630751@qq.com>
2025-12-11 21:46:04 +0800

    修正 Class6.java 中的所有错误中文字符串

--- src/com/xy/a/a/Class6.java
+++ src/com/xy/a/a/Class6.java
@@ -21,9 +21,7 @@ public class Class6 extends com.xy.q.Class30 {
    public Class6(GameView var1) {
       super(47, 2, com.xy.q.Class30.afz, var1);
       this.yy(-1, 0, 282, 335, com.xy.q.Class30.agh);
-      com.xy.w.Class9 var28 = com.xy.w.Class16.m("sc/d/1.png", 68, 68, 68, 68, false);
-      String var10004 = "冲乮核";
-      this.yu(var28, "内丹栏");
+      this.yu(com.xy.w.Class16.m("sc/d/1.png", 68, 68, 68, 68, false), "内丹栏");
       this.gj = new JLabel[5];
 
       int var2;
@@ -32,19 +30,14 @@ public class Class6 extends com.xy.q.Class30 {
          JLabel var6 = this.gj[var2];
          String var15;
          if (var2 == 0) {
-            var15 = "凬七呤秊";
             var15 = "内丹名称";
          } else if (var2 == 1) {
-            var15 = "笞\u0017w纐";
             var15 = "等  级";
          } else if (var2 == 2) {
-            var15 = "輅甥歈敊";
             var15 = "转生次数";
          } else if (var2 == 3) {
-            var15 = "弄剺纘骻";
             var15 = "当前经验";
          } else if (var2 == 4) {
-            var15 = "匮纝约骶";
             var15 = "升级经验";
          } else {
             var15 = "";
@@ -58,7 +51,6 @@ public class Class6 extends com.xy.q.Class30 {
       }
 
       this.ed = new RichLabel("", com.xy.q.Class49.w);
-      String var10006 = "tT\u0011q\u0011q\u0011q嬱乗仳浶tT\u0011qg\u0007g\u0007纔乗膽孑tT\u0011q\u0011q\u0011q纔乗膽孑ぇ孑丷亓洖〦｛维丷臝嬱Ｍ嬱乗仳浶tTg\u0007\u0011qg\u0007纔乗膽孑tT\u0011q\u0011q\u0011q｛孑丷亓洖\u00144\u0007gq\u0011\u0007g维丷臝嬱〶r\u00144q\u0011q\u0011q\u0011孑丷亓洖〵";
       this.ed.setTextSize("#cFFFFFF学习交流#cFF0000练习自学#cFFFFFF练习自学【学习交流】，练习自学：学习交流#c00FF00练习自学#cFFFFFF，学习交流#c00FF00练习自学、%#cFFFFFF学习交流。", 188);
       this.ed.setBounds(58, 128, this.ed.getWidth(), this.ed.getHeight());
       this.add(this.ed);
@@ -67,20 +59,16 @@ public class Class6 extends com.xy.q.Class30 {
       for (int var7 = var2 = 0; var7 < this.v.length; var7 = var2) {
          if (var2 < 3) {
             var7 = var2;
-            String var10005 = "I\n\u0015\f\u0015]\u0003GJ\u0007]";
             this.v[var2] = new com.xy.i.Class4("sc/e/49.png", 1, 23 + var2, this);
          } else {
             com.xy.i.Class4[] var9 = this.v;
-            var10004 = "D4\u00182\u0018e\u0001yG9P";
             int var32 = 49 + var2;
             String var33;
             Class6 var10010;
             if (var2 == 3) {
-               var33 = "輅捘约骶";
                var33 = "转换经验";
                var10010 = this;
             } else if (var2 == 4) {
-               var33 = "呇凍凒与";
                var33 = "吐出内丹";
                var10010 = this;
             } else {
@@ -109,9 +97,7 @@ public class Class6 extends com.xy.q.Class30 {
 
       for (int var11 = var2 = 0; var11 < this.fa.length; var11 = var2) {
          this.fa[var2] = com.xy.q.Class1.i(132, 53 + var2 * 23, 119, 19, 10, Color.white, com.xy.q.Class49.w);
-         com.xy.q.Class14 var12 = this.fa[var2];
-         String var20 = "I\n\u0015\r\u0015X\rGJ\u0007]";
-         var12.mt(com.xy.w.Class16.m("sc/d/17.png", 6, 6, 6, 6, false));
+         this.fa[var2].mt(com.xy.w.Class16.m("sc/d/17.png", 6, 6, 6, 6, false));
          if (var2 == 3 || var2 == 4) {
             this.fa[var2].setBounds(132, 233 + (var2 - 3) * 23, 119, 19);
          }
@@ -127,21 +113,15 @@ public class Class6 extends com.xy.q.Class30 {
          String var10003;
          if (var2 == 0) {
             var14 = this;
-            com.xy.w.Class18 var24 = this.fp[var2];
-            var10003 = "D4\u00183\u0018d\u0000yG9P";
-            var24.dp("sc/d/37.png");
+            this.fp[var2].dp("sc/d/37.png");
             this.fp[var2].setBounds(109 + 31 * var2, 18, 19, 19);
          } else if (var2 == 1) {
             var14 = this;
-            com.xy.w.Class18 var25 = this.fp[var2];
-            var10003 = "I\n\u0015\r\u0015_\nGJ\u0007]";
-            var25.mt(com.xy.w.Class16.m("sc/d/60.png", 76, 2, 76, 2, false));
+            this.fp[var2].mt(com.xy.w.Class16.m("sc/d/60.png", 76, 2, 76, 2, false));
             this.fp[var2].setBounds(46, 36, 210, 7);
          } else {
             if (var2 == 2) {
-               com.xy.w.Class18 var21 = this.fp[var2];
-               String var26 = "D4\u00183\u0018e\u0003yG9P";
-               var21.mt(com.xy.w.Class16.m("sc/d/24.png", 2, 2, 2, 2, false));
+               this.fp[var2].mt(com.xy.w.Class16.m("sc/d/24.png", 2, 2, 2, 2, false));
                this.fp[var2].setBounds(55, 127, 195, 100);
             }
 
@@ -228,27 +208,14 @@ public class Class6 extends com.xy.q.Class30 {
                String var17 = var13.getValue();
                var21 = "\u000bK";
                String[] var18 = var17.split("\\|");
-               String var10002 = var18[2];
-               String var10003 = "5\u0007";
-               String[] var6;
-               var10002 = (var6 = var10002.split("\\="))[1];
-               var10003 = "\u000b轛";
-               String[] var7 = var10002.split("\\转");
-               var21 = var18[3];
-               var10002 = "5\u0007";
-               String[] var8 = var21.split("\\=");
-               com.xy.q.Class14 var10005 = this.fa[1];
-               StringBuilder var10006 = new StringBuilder(String.valueOf(var6[1]));
-               String var10007 = "纐";
-               var10005.setText(var10006.append("级").toString());
+               String[] var6 = var18[2].split("\\=");
+               String[] var7 = var6[1].split("\\转");
+               String[] var8 = var18[3].split("\\=");
+               this.fa[1].setText(new StringBuilder(String.valueOf(var6[1])).append("级").toString());
                this.fa[2].setText(var7[0]);
                this.fa[3].setText(var8[1]);
                this.fa[4].setText(String.valueOf(this.yt().n(Integer.parseInt(var7[0]), Integer.parseInt(var7[1]) + 1)));
-               String var19 = var18[0];
-               var21 = "5\u0007";
-               String var20 = var19.split("\\=")[1];
-               var21 = "\u000b\r";
-               String[] var10 = var20.split("\\:");
+               String[] var10 = var18[0].split("\\=")[1].split("\\:");
                this.ed.setTextSize(var10[1], 188);
                if (this.zc().l(this.lj()) == null) {
                   this.zc().b(this.lj());
