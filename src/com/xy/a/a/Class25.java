266df95519f08aab570d26b9f8274f6e8b582e1e
Administrator <488630751@qq.com>
2025-12-11 22:11:41 +0800

    修正 Class25.java 中的所有错误中文字符串

--- src/com/xy/a/a/Class25.java
+++ src/com/xy/a/a/Class25.java
@@ -19,24 +19,19 @@ public class Class25 extends com.xy.q.Class30 {
    public Class25(GameView var1) {
       super(96, 2, com.xy.q.Class30.afz, var1);
       this.yy(-1, 0, 390, 365, com.xy.q.Class30.agh);
-      com.xy.w.Class9 var13 = com.xy.w.Class16.m("sc/d/1.png", 68, 68, 68, 68, false);
-      String var10005 = "鬤彁";
-      this.yu(var13, "魂归");
+      this.yu(com.xy.w.Class16.m("sc/d/1.png", 68, 68, 68, 68, false), "魂归");
       this.wq = new ArrayList<>();
       this.ly = new com.xy.i.Class4[2];
 
       int var2;
       for (int var10000 = var2 = 0; var10000 < this.ly.length; var10000 = var2) {
-         String var10006 = ")>u8uols*3=";
          this.ly[var2] = new com.xy.i.Class4("sc/e/26.png", 1, 116, com.xy.q.Class49.ch, com.xy.q.Class49.bv, "", this);
          this.ly[var2].setBounds(80 + var2 * 151, 303, 99, 25);
          com.xy.i.Class4 var4 = this.ly[var2];
          String var7;
          if (var2 == 0) {
-            var7 = "硽F讷";
             var7 = "确 认";
          } else if (var2 == 1) {
-            var7 = "厌}淒";
             var7 = "取 消";
          } else {
             var7 = "";
@@ -53,10 +48,8 @@ public class Class25 extends com.xy.q.Class30 {
          JLabel var6 = this.o[var2];
          String var10;
          if (var2 == 0) {
-            var10 = "渺駎揃祜）厉巵镈通之変乌迈蠪魑弴？";
             var10 = "温馨提示：可左键选中多个进行魂归，";
          } else if (var2 == 1) {
-            var10 = "厮镴厲亿压淒";
             var10 = "右键可以取消";
          } else {
             var10 = "";
@@ -67,12 +60,10 @@ public class Class25 extends com.xy.q.Class30 {
       }
 
       this.nn = new com.xy.w.Class18();
-      String var10004 = "`\u0005<\u0002<W*Hc\bt";
       this.nn.mt(com.xy.w.Class16.m("sc/d/19.png", 5, 5, 5, 5, false));
       this.nn.setBounds(50, 68, 309, 207);
       this.add(this.nn);
       this.wr = new com.xy.q.Class58(this, 6, 4, 51, 51, 0, 0, 51, 70);
-      var10004 = ")>u9ulbs*3=";
       this.wr.gt(com.xy.w.Class16.c("sc/d/18.png"));
       this.add(this.wr);
    }
@@ -86,7 +77,6 @@ public class Class25 extends com.xy.q.Class30 {
    public void g(int var1) {
       if (var1 == 116) {
          if (this.wq.size() == 0) {
-            String var10001 = "诤遯拺膕専书丹觧魑弴皗晹卲";
             this.afx.dp("请选择至少一个要魂归的星卡");
          } else {
             SuitOperBean var2 = new SuitOperBean();
