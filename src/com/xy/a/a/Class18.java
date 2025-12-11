2b0d12f170b85fb41021c050fe751d36c12ff0a7
Administrator <488630751@qq.com>
2025-12-11 22:03:35 +0800

    修正 Class18.java 中的所有错误中文字符串

--- src/com/xy/a/a/Class18.java
+++ src/com/xy/a/a/Class18.java
@@ -28,11 +28,7 @@ public class Class18 extends com.xy.q.Class30 {
    public Class18(GameView var1) {
       super(173, 3, com.xy.q.Class30.afz, var1);
       this.yy(-1, 0, 282, 345, com.xy.q.Class30.agh);
-      com.xy.w.Class9 var6 = com.xy.w.Class16.m("sc/d/1.png", 68, 68, 68, 68, false);
-      String var10007 = "逎拴块验";
-      this.yu(var6, "选择坐骑");
-      String var10008 = "\u001aQFWF\u0005GB\u0007U";
-      String var10013 = "硩宇";
+      this.yu(com.xy.w.Class16.m("sc/d/1.png", 68, 68, 68, 68, false), "选择坐骑");
       this.mn = new com.xy.i.Class4("sc/e/7.png", 1, 201, com.xy.q.Class49.ch, com.xy.q.Class49.bv, "确定", this);
       this.mn.setBounds(124, 297, 59, 25);
       this.add(this.mn);
@@ -55,15 +51,11 @@ public class Class18 extends com.xy.q.Class30 {
          Class18 var3;
          if (var2 == 0) {
             var3 = this;
-            com.xy.w.Class18 var10002 = this.cl[var2];
-            String var10003 = "A\n\u001d\r\u001d[\u0007GB\u0007U";
-            var10002.mt(com.xy.w.Class16.m("sc/d/25.png", 2, 2, 2, 2, false));
+            this.cl[var2].mt(com.xy.w.Class16.m("sc/d/25.png", 2, 2, 2, 2, false));
             this.cl[var2].setBounds(52, 38, 200, 21);
          } else {
             if (var2 == 1) {
-               com.xy.w.Class18 var10001 = this.cl[var2];
-               String var4 = "nd2c25))miz";
-               var10001.mt(com.xy.w.Class16.m("sc/d/24.png", 2, 2, 2, 2, false));
+               this.cl[var2].mt(com.xy.w.Class16.m("sc/d/24.png", 2, 2, 2, 2, false));
                this.cl[var2].setBounds(52, 38, 200, 250);
             }
 
@@ -85,34 +77,19 @@ public class Class18 extends com.xy.q.Class30 {
          RoleData var2 = this.yx();
          Mount var3 = this.ro.getSelectedIndex() >= 0 ? var2.mounts.get(this.ro.getSelectedIndex()) : null;
          if (var3 == null) {
-            String var10001 = "沓杠逻乄皶圹骣";
             this.afx._do("没有选中的坐骑");
             return;
          }
 
          this.zc().f(this.lj());
          if (this.cn == 0) {
-            GameView var4 = this.afx;
-            String var15 = "TU@G";
-            StringBuilder var7 = new StringBuilder("SHGZ").append(var3.getMountid());
-            String var12 = "N";
-            var7 = var7.append("|").append(this.cn);
-            var12 = "a";
-            String var9 = var7.append("|").append(this.ae).toString();
-            var12 = "Je圹骣禒兗寡抖榔吼･叝Ju菞徥寡抖瞚劒捨\u0011>〰圹骣禒兗寡抖榔露觨芃赐\u0011.\u0001YeJe钟世E硜词逻技歖圹骣呾Ｍ";
-            var4.dm(new ConfirmBean(32, var9, "#W坐骑移入守护槽后，可#G获得守护石加持#W。坐骑移入守护槽需要花费#G30W#W银两,确认选择此坐骑吗？"));
+            StringBuilder var4 = new StringBuilder("SHGZ").append(var3.getMountid()).append("|").append(this.cn).append("|").append(this.ae);
+            this.afx.dm(new ConfirmBean(32, var4.toString(), "#W坐骑移入守护槽后，可#G获得守护石加持#W。坐骑移入守护槽需要花费#G30W#W银两,确认选择此坐骑吗？"));
             return;
          }
 
-         GameView var10000 = this.afx;
-         String var10006 = "TU@G";
-         StringBuilder var10004 = new StringBuilder("SHGZ").append(var3.getMountid());
-         String var10005 = "N";
-         var10004 = var10004.append("|").append(this.cn);
-         var10005 = "a";
-         String var6 = var10004.append("|").append(this.ae).toString();
-         var10005 = "Je砇讖遠招欍坢髸秉儌宺拍槏呾Ｍ";
-         var10000.dm(new ConfirmBean(32, var6, "#W确认选择此坐骑移入守护槽吗？"));
+         StringBuilder var5 = new StringBuilder("SHGZ").append(var3.getMountid()).append("|").append(this.cn).append("|").append(this.ae);
+         this.afx.dm(new ConfirmBean(32, var5.toString(), "#W确认选择此坐骑移入守护槽吗？"));
       }
    }
 
